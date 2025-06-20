0751313bfe98dfaec6eccf99aae3e680f5ffe337
notmansingh <notamansingh@gmail.com>
2025-06-20 07:44:29 +0000

    [automatic commit] 2025-06-20 07:44:28 UTC 49.178.103.128

--- part1/app.js
+++ part1/app.js
@@ -69,8 +69,8 @@ let db;
         INSERT INTO Dogs (owner_id, name, size)
         VALUES
           ((SELECT user_id FROM Users WHERE username='alice123'), 'Max', 'medium'),
-          ((SELECT user_id FROM Users WHERE username='carol123'), 'Bella', 'small')
-          
+          ((SELECT user_id FROM Users WHERE username='carol123'), 'Bella', 'small'),
+          ((SELECT user_id FROM Users WHERE username='carol123'), 'Dennis', 'Large')
       `);
       await db.query(`
         INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location, status)
