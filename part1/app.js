bb787538c4fef9be88a0864c74720f2cbdef7d68
notmansingh <notamansingh@gmail.com>
2025-06-20 07:49:24 +0000

    [automatic commit] 2025-06-20 07:49:23 UTC 49.178.103.128

--- part1/app.js
+++ part1/app.js
@@ -79,7 +79,7 @@ let db;
         INSERT INTO WalkRatings (request_id, walker_id, owner_id, rating, comments)
         VALUES
           (
-            2,
+            1,
             (SELECT user_id FROM Users WHERE username='bobwalker'),
             (SELECT user_id FROM Users WHERE username='carol123'),
             4,
