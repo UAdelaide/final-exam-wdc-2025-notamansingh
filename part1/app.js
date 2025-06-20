f316454197cdc5d674c05223d918deefea3d204e
notmansingh <notamansingh@gmail.com>
2025-06-20 07:45:15 +0000

    [automatic commit] 2025-06-20 07:45:14 UTC 49.178.103.128

--- part1/app.js
+++ part1/app.js
@@ -68,8 +68,6 @@ let db;
       await db.query(`
         INSERT INTO Dogs (owner_id, name, size)
         VALUES
-          ((SELECT user_id FROM Users WHERE username='alice123'), 'Max', 'medium'),
-          ((SELECT user_id FROM Users WHERE username='carol123'), 'Bella', 'small'),
           ((SELECT user_id FROM Users WHERE username='carol123'), 'Dennis', 'Large')
       `);
       await db.query(`
